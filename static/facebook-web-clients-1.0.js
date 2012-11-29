fd0fb5903f70d6eb914847d01c53f32ab789781b
Olly Oechsle <Password4>
2012-11-29 12:04:32 +0000

    Correct colours, and joined up properly

--- static/facebook-web-clients-1.0.js
+++ static/facebook-web-clients-1.0.js
@@ -17,43 +17,51 @@ guardian.facebook = {};
     Donut.prototype.initialise = function () {
         var width = this.jContainer.width(),
             height = this.jContainer.height(),
-            halfWidth = width / 2,
-            R = width / 2;
+            centerX = width / 2,
+            radius = (width - 24) / 2;
 
         this.paper = new window.Raphael(this.jContainer[0], width, height);
-        this.paper.customAttributes.arc = function (value, total) {
-            var alpha = 360 / total * value,
+        this.paper.customAttributes.arc = function (percent) {
+            var alpha = 360 /  100 * percent,
                 a = (90 - alpha) * Math.PI / 180,
-                x = halfWidth + R * Math.cos(a),
-                y = halfWidth - R * Math.sin(a),
+                x = centerX + radius * Math.cos(a),
+                y = centerX - radius * Math.sin(a),
                 path;
-            if (total == value) {
+            if (percent == 100) {
                 path = [
-                    ["M", halfWidth, halfWidth - R],
-                    ["A", R, R, 0, 1, 1, halfWidth - 0.01, halfWidth - R]
+                    ["M", centerX, centerX - radius],
+                    ["A", radius, radius, 0, 1, 1, centerX - 0.01, centerX - radius]
                 ];
             } else {
                 path = [
-                    ["M", halfWidth, halfWidth - R],
-                    ["A", R, R, 0, +(alpha > 180), 1, x, y]
+                    ["M", centerX, centerX - radius],
+                    ["A", radius, radius, 0, +(alpha > 180), 1, x, y]
                 ];
             }
             return {path: path};
+        };
+        this.paper.customAttributes.turn = function (percent) {
+            angle = 360 / 100 * percent;
+            return ({"transform": ["R" + angle, centerX, centerX].join()});
         };
     };
 
     Donut.prototype.render = function () {
         this.paper.clear();
-        this.paper.path()
-            .attr(Donut.NEGATIVE)
-            .attr({arc: [30, 60]});
+
         this.paper.path()
             .attr(Donut.POSITIVE)
-            .attr({arc: [30, 60]});
+            .attr({arc: [25]})
+            .attr({turn: [0]});
+
+        this.paper.path()
+            .attr(Donut.NEGATIVE)
+            .attr({arc: [75]})
+            .attr({turn: [25]});
     };
 
-    Donut.NEGATIVE = {stroke: "#f00", "stroke-width": 30};
-    Donut.POSITIVE = {stroke: "#0f0", "stroke-width": 30};
+    Donut.NEGATIVE = {stroke: "#3A7D00", "stroke-width": 24};
+    Donut.POSITIVE = {stroke: "#0D3D00", "stroke-width": 24};
 
     guardian.facebook.Donut = Donut;
 
